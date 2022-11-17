4dac0f3b2a574f2810b0bb866c8a8a35d9409f06
DaveatCor <condaveat@gmail.com>
2022-11-17 16:14:36 +0700

    connect with js, 17/Nov/22

---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+
+    if (swAvailable && swInterceptAvailable) {
+      AndroidServiceWorkerController serviceWorkerController =
+          AndroidServiceWorkerController.instance();
+
+      await serviceWorkerController
+          .setServiceWorkerClient(AndroidServiceWorkerClient(
+        shouldInterceptRequest: (request) async {
+          print(request);
+          return null;
+        },
+      ));
+    }
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  late PullToRefreshController pullToRefreshController;
+  String url = "";
+  double progress = 0;
+  final urlController = TextEditingController();
+
+  HeadlessInAppWebView? _headlessInAppWebView;
+
+  void _incrementCounter() async {
+    // await _headlessInAppWebView!.webViewController.evaluateJavascript(source: "/Users/daveat/Documents/webview_flutter/typescript/src/index.js").then((value) {
+    //   print("_headlessInAppWebView webViewController $value");
+    // });
+    await rootBundle.loadString("typescript/dist/main.js").then((value) async {
+      print("value $value");
+
+      await webViewController!.evaluateJavascript(source: 
+        value
+      ).then((evaluateJavascript) {
+        print("evaluateJavascript $evaluateJavascript");
+      });
+    });
+    // (handlerName: 'handlerFoo', callback: (args) {
+    //   print(args);
+    //   // return data to the JavaScript side!
+    //   return {
+    //     'bar': 'bar_value', 'baz': 'baz_value'
+    //   };
+    // });
+  }
+
+  @override
+  void initState() {
+    _headlessInAppWebView = HeadlessInAppWebView();
+    super.initState();
+  }
+  
+  @override
+  void dispose(){
+    _headlessInAppWebView?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: InAppWebView(
+        initialData: InAppWebViewInitialData(
+          data: """
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                </head>
+                <body>
+                    <h1>JavaScript Handlers</h1>
+                    <script>
+                        window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
+                            window.flutter_inappwebview.callHandler('handlerFoo')
+                              .then(function(result) {
+                                // print to the console the data coming
+                                // from the Flutter side.
+                                console.log(JSON.stringify(result));
+                                
+                                window.flutter_inappwebview
+                                  .callHandler('handlerFooWithArgs', 1, true, ['bar', 5], {foo: 'baz'}, result);
+                            });
+                        });
+                    </script>
+                </body>
+            </html>
+          """
+        ),
+        initialOptions: options,
+        onWebViewCreated: (controller) async {
+          
+          await controller.android.clearHistory();
+          print("controller ${controller.ios}");
+          webViewController = controller;
+          // webViewController!.addJavaScriptHandler(handlerName: 'handlerFoo', callback: (args) {
+          //   // return data to the JavaScript side!
+          //   return {
+          //     'bar': 'bar_value', 'baz': 'baz_value'
+          //   };
+          // });
+
+          // webViewController!.addJavaScriptHandler(handlerName: 'handlerFooWithArgs', callback: (args) {
+          //   print(args);
+          //   // it will print: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}]
+          // });
+        },
+        
+        onConsoleMessage: (controller, consoleMessage) {
+          print(consoleMessage);
+          // it will print: {message: {"bar":"bar_value","baz":"baz_value"}, messageLevel: 1}
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
